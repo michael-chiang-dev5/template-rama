c79ac6aa213e305931ae4b574c18cbadab4a5ee3
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-02-11 15:38:21 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './client/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  entry: '/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/build': 'http://localhost:3000',
+    },
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /(node_modules)/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
