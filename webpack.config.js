fdbbeca8af3fe83c00ad5d9c06effde4e863f6bd
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-03 15:39:51 -0800

    CHORE: adds typescript to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,26 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, './client/index.html'),
-      filename: 'index.html',
-    }),
-  ],
-  entry: '/client/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/build': 'http://localhost:3000',
-    },
-    compress: true,
-    port: 8080,
-    historyApiFallback: true,
-  },
+  entry: './client/index.ts',
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        exclude: /(node_modules)/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.png|svg|jpg|gif$/,
-        use: ['file-loader'],
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 4000,
   },
 };
