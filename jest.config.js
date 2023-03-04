9a4dbcb9ab1fa414f5d2188a8d0325706dcca3bb
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-04 03:10:30 -0800

    FIX: adds css mocks for jest testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // When Jest hits a css import, it tries parse it like css is javascript which does not work
+  // We get around this by mocking css files with empty javascript
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
