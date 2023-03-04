bdc1e7dc85a2c095dc6afa8ee0c4d4f6bf912c66
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-04 01:14:27 -0800

    REFACTOR: renames declaration file for css into something more informative

--- client/types/css.d.ts
+++ client/types/css.d.ts
@@ -1,4 +1,3 @@
 // this is required for css to work
 // https://stackoverflow.com/questions/40382842/cant-import-css-scss-modules-typescript-says-cannot-find-module
-// TODO: consider using npm package `typescript-plugin-css-modules`
 declare module '*.module.css';
