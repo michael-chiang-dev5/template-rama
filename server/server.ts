70b8377684c122ef1e35f438c843f74da5963132
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-04 00:38:00 -0800

    FIX: production build now served on localhost:8080/

--- server/server.ts
+++ server/server.ts
@@ -10,8 +10,14 @@ import path from 'path';
 const app = express();
 const PORT = 8080;
 
-// serve index.html
-app.get('/', (req, res) => {
+/*
+ * Serves production build on route: localhost:8080
+ * You must first build production files via: npm run build
+ */
+// serves static files bundle.css, bundle.js
+app.use(express.static('dist'));
+// serves index.html
+app.get('/', (req: Request, res: Response) => {
   return res
     .status(200)
     .sendFile(path.resolve(__dirname, '../dist/index.html'));
