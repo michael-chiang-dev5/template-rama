b2bc9b73c42cd614a5b5656646ca3eaee17c60b9
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-03 23:36:16 -0800

    FIX: CSS now displays properly without the need for console.log to force load

--- client/App.tsx
+++ client/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import styles from './index.module.css';
 
 function App() {
-  console.log(styles)
-  return <h1>Hello World!</h1>;
+  return <h1 className={styles.pink}>Hello World!</h1>;
 }
 
 export default App;
