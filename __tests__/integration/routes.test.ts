9ad0998eddd909e44c242407d0b7607d454051bd
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-05 00:11:24 -0800

    FIX: fixed ts errors in tests

--- __tests__/integration/routes.test.ts
+++ __tests__/integration/routes.test.ts
@@ -2,16 +2,13 @@ import request from 'supertest';
 import { appCreator } from '../../src/server/appCreator';
 
 describe('Auth endpoints', () => {
-  let app;
-  beforeEach(() => {
-    app = appCreator();
-  });
-  afterEach(() => {});
   it('GET "/api" response is 200', async () => {
+    const app = appCreator();
     const response = await request(app).get('/api');
     expect(response.status).toEqual(200);
   });
   it('GET "/nonexistent" response is 404', async () => {
+    const app = appCreator();
     const response = await request(app).get('/nonexistent');
     expect(response.status).toEqual(404);
   });
