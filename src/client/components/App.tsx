a9f50cc28f02def166c64f4a439c4122204376a1
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-03-05 02:39:08 -0800

    testing ci/cd

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './index.module.css';
 
 function App() {
-  return <h1 className={'styles.pink'}>Hello World!</h1>;
+  return <h1 className={'styles.pink'}>Testing ci/cd</h1>;
 }
 
 export default App;
